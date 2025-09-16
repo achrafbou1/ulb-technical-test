098573637ac215ec5d1d5d3d26f2a65838334f6f
Achraf Boutat <a.boutat4@gmail.com>
2025-09-16 16:26:07 +0200

    fix: fix run commands

--- partie-1/models/inscription.py
+++ partie-1/models/inscription.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, Column, Text
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from db import Base
 
@@ -7,8 +7,8 @@ from db import Base
 class Inscription(Base):
     __tablename__ = "liste_inscriptions"
 
-    annee_etude: Mapped[int] = Column(Integer, primary_key=True)
-    matricule: Mapped[str] = Column(Text, primary_key=True)
-    cours_json: Mapped[str] = Column(Text, nullable=False)
-    nom: Mapped[str] = Column(Text, nullable=False)
-    prenom: Mapped[str] = Column(Text, nullable=False)
+    annee_etude: Mapped[int] = mapped_column(Integer, primary_key=True)
+    matricule: Mapped[str] = mapped_column(Text, primary_key=True)
+    cours_json: Mapped[str] = mapped_column(Text, nullable=False)
+    nom: Mapped[str] = mapped_column(Text, nullable=False)
+    prenom: Mapped[str] = mapped_column(Text, nullable=False)
